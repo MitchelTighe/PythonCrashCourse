c9c18018542e5d2225a420b4d2b81c960c7b6b99
Mitchel Tighe <mtighe@redlineperf.com>
2022-07-19 20:07:59 -0400

    AWWD

--- chap8.3.py
+++ chap8.3.py
@@ -4,6 +4,7 @@ def shirts(size='large', text='I Love Python'):
     print(f"Creating your shirt in size {size}, with the message {text}.")
 
 shirts()
+#
 shirts(size='medium')
 
 shirts(size='small', text='I love perl')
