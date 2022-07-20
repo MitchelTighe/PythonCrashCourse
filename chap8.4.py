9d054d01a86cb4a3e3f26027450d33fcb15b4a0f
Mitchel Tighe <Mitchel.Tighe@noaa.gov>
2022-07-20 10:42:53 -0400

    Chap 8.4 being added

---
+++ chap8.4.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+def format_cities(city, country):
+    name = f"{city.title()}, {country.title()}"
+    return name.title()
+
+cities_name = format_cities('Washington DC', 'United States of America')
+print(cities_name)
+
+def city_dict(city, country):
+    formatted = {'city': city, 'country': country}
+    return formatted
+
+test_build = city_dict('boulder', 'united states of america')
+print(test_build)
+
